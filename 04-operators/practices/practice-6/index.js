2246339c6b4dd1a02770329a933602075d15cbc8
Olga Volska <volskavideo@gmail.com>
2023-01-05 16:50:51 +0300

    complete practices of operators

---
+++ 04-operators/practices/practice-6/index.js
@@ -0,0 +1,5 @@
+let num = Number(prompt('num:'));
+
+console.log(`Is greater: ${num > Math.PI}`);
+console.log(`Is less: ${num < Math.PI}`);
+console.log(`Is equal: ${num === Math.PI}`);
---
+++ 04-operators/practices/practice-7/index.js
@@ -0,0 +1,9 @@
+let uzerNumb = prompt('x:');
+let cube = Math.pow(uzerNumb, 3);
+let sqrt = Math.sqrt(uzerNumb);
+let result = (cube - sqrt);
+
+console.log(`x = ${uzerNumb}`);
+console.log(`cube = ${cube}`);
+console.log(`sqrt = ${sqrt}`);
+console.log(`result = ${result}`);
